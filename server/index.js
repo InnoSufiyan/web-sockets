dda284a9c3af19a1495043bdaa3df8e5cd97b2b2
Inno Sufiyan <66431140+InnoSufiyan@users.noreply.github.com>
2024-05-15 19:42:58 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,37 +1,41 @@
-import express from 'express';
-import { createServer } from 'http';
+import express from "express";
+import { createServer } from "http";
 import { Server } from "socket.io";
-import cors from 'cors';
+import cors from "cors";
 const app = express();
 const server = createServer(app);
 app.use(cors());
 const io = new Server(server, {
-    cors: {
-        origin: '*',
-    }
-})
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+  cors: {
+    origin: "*",
+  },
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    console.log(socket.id);
-    //.emit is used to send message to the client
-    socket.emit('me', `Hi , ${socket.id} , welcome to the app`);
-    //socket.broadcast.emit is used to send message to all the clients except the sender
-    socket.broadcast.emit('new-user', `New user joined ${socket.id}`);
-    socket.on('message', (message) => {
-        console.log(message);
-        //socket.broadcast.emit is used to send message to all the clients except the sender
-        socket.broadcast.emit('message', message);
-    });
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  console.log(socket.id);
+  //.emit is used to send message to the client
+  socket.emit("me", `Hi , ${socket.id} , welcome to the app`);
+  //socket.broadcast.emit is used to send message to all the clients except the sender
+  socket.broadcast.emit("new-user", `New user joined ${socket.id}`);
+  socket.on("message", ({ data, room }) => {
+    // console.log(message);
+    //socket.broadcast.emit is used to send message to all the clients except the sender
+    if (!room) {
+      socket.broadcast.emit("message", data);
+    } else {
+      io.to(room).emit("message", data);
+    }
+  });
+  socket.on("create-room", (roomName) => {
+    socket.join(roomName);
+  });
 });
 
 server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
-
